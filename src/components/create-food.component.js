5554af37bcfa7b577e90122ce35a7ae803163ee9
Nhung Tran <nhungtrannn@gmail.com>
2019-04-12 17:17:52 -0500

    initial commit

--- src/components/create-food.component.js
+++ src/components/create-food.component.js
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
 
 export default class CreateFood extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            food_description: '',
+            food_responsible: '',
+            food_priority: '',
+            food_completed: false
+        }
+    }
+
+    onChangeFoodDescription(e) {
+        this.setState({
+            food_description: e.target.value 
+        })
+    }
+ 
     render() {
         return (
             <div>
